776147d80ef113abb07823540d4f70e89bfb4f0f
Jobin Joseph <jobin497@gmail.com>
2017-04-21 23:11:12 +0300

    Pushing Cloudflare-DynamicDNS.sh

---
+++ Cloudflare-DynamicDNS.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#Author : redhatjobin
+
+############## Add your CloudFlare Credentials Here ##############
+
+auth_email=your-login-id@gmail.com   # Your Login email id
+auth_key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX # Get this from CloudFlare MyAccount Section
+zone_name=example.com   # Your Website name in CloudFlare eg: example.com
+record_name=ddns           # A record name for DynamicDNS eg: ddns
+
+
+###################################################################
+
+identify () {
+zone_identifier=`curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" | cut -d'"' -f6`
+
+record_identifier=`curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name.$zone_name" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json"  | cut -d'"' -f6`
+
+if [ $record_identifier == "page" ] ; then
+    echo "Zone Does not exist, Dont Worry, I will create it for you :)"
+    createzone
+else
+    echo " "
+fi
+
+}
+#echo $auth_email , $auth_key , $zone_name , $record_name , $zone_identifier , $record_identifier
+
+createzone () {
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" --data '{"type":"A","name":"'$record_name'","content":"'$ipnow'","ttl":120,"proxied":false}'     
+}
+
+dnschange () {
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" --data '{"type":"A","name":"'$record_name'","content":"'$ipnow'","ttl":120,"proxied":false}'
+}
+
+
+ipnow=`dig +short myip.opendns.com @resolver1.opendns.com`
+iplive=`cat /tmp/mypublicip`
+
+if [ "$iplive" == "$ipnow" ];then
+  echo "No change in IP - Live : $iplive Now : $ipnow"
+else 
+    echo "IP change is required from $iplive to $ipnow"
+    identify
+    dnschange
+    echo $ipnow > /tmp/mypublicip
+fi
+
+exit 0
